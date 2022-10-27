8837f3e9aa1f241b7e3775216163f5baf48ea37b
Nicolas <nicolas.murillo@stratesys-ts.com>
2022-10-26 23:34:36 -0500

    Se configuro la interaz para el analizador lexico

---
+++ src/main/kotlin/co/edu/uniquindio/compiladores/proyecto/controladores/InicioController2.kt
@@ -0,0 +1,80 @@
+package co.edu.uniquindio.compiladores.proyecto.controladores
+
+import co.edu.uniquindio.compiladores.proyecto.lexico.AnalizadorLexico
+import co.edu.uniquindio.compiladores.proyecto.lexico.Token
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.scene.control.Button
+import javafx.scene.control.TableColumn
+import javafx.scene.control.TableView
+import javafx.scene.control.TextArea
+import javafx.scene.control.cell.PropertyValueFactory
+
+class InicioController2 {
+
+    @FXML lateinit var botonAnalizar: Button
+    @FXML lateinit var tokens: TableView<Token>
+    @FXML lateinit var lexema: TableColumn<Token, String>
+    @FXML lateinit var categoria: TableColumn<Token, String>
+    @FXML lateinit var fila: TableColumn<Token, String>
+    @FXML lateinit var columna: TableColumn<Token, String>
+
+    @FXML lateinit var tableResult: TableView<Token>
+    @FXML lateinit var columnLex: TableColumn<Token, String>
+    @FXML lateinit var columnCategory: TableColumn<Token, String>
+    @FXML lateinit var columnRow: TableColumn<Token, String>
+    @FXML lateinit var columnColumn: TableColumn<Token, String>
+
+    private var analizadorLexico: AnalizadorLexico? = null
+
+    private val data: ObservableList<Token> = FXCollections.observableArrayList()
+
+    @FXML lateinit var codigoFuente:TextArea
+    @FXML
+    fun analizarLexema(e:ActionEvent)
+    {
+        if(codigoFuente.text.isNotEmpty())
+        {
+            println("Cantidad de caracteres: ${codigoFuente.length}")
+            analizadorLexico = AnalizadorLexico(codigoFuente.text)
+            analizadorLexico!!.analizar()
+            addDataInTable(analizadorLexico!!.listaTokens)
+            analizadorLexico!!.listaTokens.forEach { t ->
+                println(t)
+            }
+            val lexico = AnalizadorLexico(codigoFuente.text)
+            lexico.analizar()
+
+            print (lexico.listaTokens)
+        }
+
+    }
+    fun processWords(codigoFuente: String) {
+
+        println("Cantidad de caracteres: ${codigoFuente.length}")
+        analizadorLexico = AnalizadorLexico(codigoFuente)
+        analizadorLexico!!.analizar()
+        addDataInTable(analizadorLexico!!.listaTokens)
+        analizadorLexico!!.listaTokens.forEach { t ->
+            println(t)
+        }
+    }
+    private fun addDataInTable(listaTokens: ArrayList<Token>) {
+
+        if(data.isNotEmpty()) data.clear()
+
+        lexema.cellValueFactory = PropertyValueFactory("lexema")
+        categoria.cellValueFactory = PropertyValueFactory("categoria")
+        fila.cellValueFactory = PropertyValueFactory("fila")
+        columna.cellValueFactory = PropertyValueFactory("columna")
+
+
+        listaTokens.forEach { token -> tokens.items.add(token) }
+//        tokens.items = data
+        println(tokens.items)
+        tokens.refresh()
+    }
+
+}
